ad8215d54126fe386b4b24fd76c881494dc2e083
Jesus Cano <jcanot1300@alumno.ipn.mx>
2018-05-04 17:16:47 -0500

    clase 156

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
-  templateUrl: './template.component.html',
-  styleUrls: ['./template.component.css']
+  templateUrl: './template.component.html'
 })
 export class TemplateComponent implements OnInit {
+
+  usuario: Object = {
+    nombre: "Jesus Jaime",
+    apellido: "Cano Terrazas",
+    correo: "jesuscanoterrazas@outlook.com"
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  guardar( forma:NgForm ){
+    console.log("NgForm", forma);
+    console.log("Valor", forma.value);
+
+    console.log("Usuario desde los inputs", this.usuario);
+    
   }
 
 }
