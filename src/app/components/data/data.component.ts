cec7c1554801bf58f4a0da83494ff29c6be681ce
JesusJaimeCano <jcanot1300@alumno.ipn.mx>
2018-05-06 23:30:07 -0500

    medio video 163, aproximacion por data

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+  
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
 
-  constructor() { }
+  forma: FormGroup; //Responsable del manejo de la forma completamente
 
-  ngOnInit() {
+  constructor() {
+    //Creamos un nuevo formGroup, pero este recibe un objeto de los elementos que tendra dentro del mismo
+    //por ejemplo nombre y este mismo al declararlo lleva parametros, como lo son 
+    //Valor por defecto,  regla de validacion (si es mas de una, se pone dentro del arreglo), reglas de validacion asyncrona
+    this.forma = new FormGroup({
+      'nombre': new FormControl('Jesús'),
+      'apellido': new FormControl(),
+      'correo': new FormControl()
+    });
+
+    //Se necesita ligar los elementos del html al ts, 
+
   }
 
 }
